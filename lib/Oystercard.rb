e5722133d9dc7302ee33b72822ab8e0112db62ca
Anna Lucking <annavlucking@hotmail.co.uk>
2016-10-04 17:27:00 +0100

    Refactor minimum balance

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -2,6 +2,7 @@ class Oystercard
 
   attr_reader :balance
   MAXIMUM_LIMIT = 90
+  MINIMUM_BALANCE = 1
 
   def initialize
     @balance = 0
@@ -18,7 +19,7 @@ class Oystercard
   end
 
   def touch_in
-    raise "Insufficient funds" if @balance < 1
+    raise "Insufficient funds" if @balance < MINIMUM_BALANCE
     @in_journey = true
   end
 
