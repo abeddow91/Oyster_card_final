2ca87f83a8510c21f9b07bd621adfa6c4f7fbd73
Michelle Epstein <epstein.michelle@gmail.com>
2016-10-04 13:21:49 +0100

    Enforced maximum value of 90

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
   attr_reader :balance
+  MAXIMUM_LIMIT = 90
 
 def initialize
   @balance = 0
 end
 
 def top_up(amount)
+  raise "The maximum top up value of #{MAXIMUM_LIMIT} has been reached!" if @balance + amount > MAXIMUM_LIMIT
   @balance += amount
 end
 
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -9,4 +9,10 @@ end
 it "should top up the card" do
   expect(subject.top_up(5)).to eq 5
 end
+
+it "should raise an error if the top up limit is reached" do
+  maximum = Oystercard::MAXIMUM_LIMIT
+  subject.top_up(maximum)
+  expect {subject.top_up(1)}.to raise_error("The maximum top up value of #{maximum} has been reached!")
+end
 end
